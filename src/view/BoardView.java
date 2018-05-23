0dd0cc81eed2875b1fae6f11d7b127c636313776
MattPoly <matthieu.cayet@etu.univ-lyon1.fr>
2018-05-23 14:35:39 +0200

    update BoardView

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -6,11 +6,17 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -27,19 +33,45 @@ public class BoardView extends BorderPane implements EventHandler, Observer {
         this.plateau = plateau;
         plateau.addObserver(this);
 
+        this.loadHomeView();
+    }
 
+    private void loadHomeView() {
         this.setTop(this.getMenuBar());
-        this.setCenter(new DefaultView());
+        this.setCenter(this.getListGamesView());
         this.getCenter().setId("GameView");
+    }
+
+    private Pane getListGamesView() {
+        VBox listGamesView = new VBox();
+        listGamesView.setAlignment(Pos.CENTER);
+        listGamesView.setSpacing(10);
+
+        Text title = new Text("Available games: ");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 18));
+        listGamesView.getChildren().add(title);
+
+        Text tetris = new Text("Tetris");
+        tetris.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+        listGamesView.getChildren().add(tetris);
+
+
+        Text blokus = new Text("Blokus (soon)");
+        blokus.setFont(Font.font("Arial", FontPosture.ITALIC, 14));
+        listGamesView.getChildren().add(blokus);
+
+        return listGamesView;
     }
 
     private MenuBar getMenuBar() {
 
         MenuBar menuBar = new MenuBar();
         Menu boardMenu = new Menu("Board");
+        MenuItem home = new MenuItem("Home");
+        home.setOnAction(this);
         MenuItem quit = new MenuItem("Quit");
         quit.setOnAction(this);
-        boardMenu.getItems().add(quit);
+        boardMenu.getItems().addAll(home,quit);
         menuBar.getMenus().add(boardMenu);
 
         // Games Menu
@@ -65,6 +97,9 @@ public class BoardView extends BorderPane implements EventHandler, Observer {
         if (event instanceof ActionEvent && event.getSource() instanceof MenuItem){
             String source = ((MenuItem)event.getSource()).getText();
             switch (source) {
+                case "Home":
+                    this.loadHomeView();
+                    break;
                 case "Quit":
                     Platform.exit();
                     break;
